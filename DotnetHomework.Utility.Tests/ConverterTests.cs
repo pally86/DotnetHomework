d50addaded808d550f07ccfe0802ed305b05e29c
pally86 <pavol.hlavac86@gmail.com>
2023-02-13 13:39:25 +0100

    upravy

--- DotnetHomework.Utility.Tests/ConverterTests.cs
+++ DotnetHomework.Utility.Tests/ConverterTests.cs
@@ -1,4 +1,4 @@
-﻿using DotnetHomework.Models;
+using DotnetHomework.Models;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -12,13 +12,13 @@ namespace DotnetHomework.Utility
     [TestFixture]
     public class ConverterTests
     {
-        private Document document1;
+        private Document _document1;
         private Converter _converter;
-        private Result result;
+        private Result _result;
         [SetUp]
         public void Setup()
         {
-            document1 = new Document()
+            _document1 = new Document()
             {
                 Id = "TestId1",
                 Data = new object[] { new { prop1 = "prop1", prop2 = 1 }, new { prop3 = "prop3", prop4 = 4 } },
@@ -32,9 +32,9 @@ namespace DotnetHomework.Utility
         [TestCase("messagepack", ExpectedResult = "application/x-msgpack")]
         public async Task<string> Convert_Document_GetContentType(string typeFile)
         {
-            var jsonString = JsonSerializer.Serialize(document1);
-            result = await _converter.Convert(jsonString, typeFile);
-            return result.ContentType;
+            var jsonString = JsonSerializer.Serialize(_document1);
+            _result = await _converter.Convert(jsonString, typeFile);
+            return _result.ContentType;
         }       
     }
 }
